88ca6fb9a77e4e61a325dce81674c0ef4ee7b82c
Gabriel Coelho <gaaucocoelho@gmail.com>
2024-01-06 16:17:05 -0300

    add initial data seeding for prod env

--- node/src/infra/database/setupSchema.js
+++ node/src/infra/database/setupSchema.js
@@ -1,12 +1,26 @@
+import { env } from 'node:process'
 import { pool } from '#infra/database/index'
 
 export default function setupDatabaseSchema() {
-  const sql = `
-    CREATE TABLE IF NOT EXISTS person (
-      id INT NOT NULL AUTO_INCREMENT,
-      first_name VARCHAR(255),
-      last_name VARCHAR(255),
-      PRIMARY KEY (id)
-    )`
-  return pool.query(sql)
+  // SQL to create table
+  const createTableSql = `
+   CREATE TABLE IF NOT EXISTS person (
+     id INT NOT NULL AUTO_INCREMENT,
+     first_name VARCHAR(255),
+     last_name VARCHAR(255),
+     PRIMARY KEY (id),
+     UNIQUE KEY unique_person (first_name, last_name)
+   );`
+
+  // SQL to insert a single record
+  const insertRecordSql = `
+    INSERT INTO person (first_name, last_name) VALUES ('John', 'Doe') ON DUPLICATE KEY UPDATE id=id;
+  `
+
+  let script = createTableSql
+
+  if (env.NODE_ENV === 'production')
+    script += insertRecordSql
+
+  return pool.query(script)
 }
